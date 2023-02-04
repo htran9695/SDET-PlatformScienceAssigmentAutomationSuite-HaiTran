1208aab2e2d0454fb6e52ec0866df8476e2e6950
Hai Tran <htran9695@gmail.com>
2023-02-04 16:12:03 -0500

    Update comments for Coordinates and RoomBuilder

--- src/test/java/utils/Coordinates.java
+++ src/test/java/utils/Coordinates.java
@@ -7,11 +7,21 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * This is the custome coordinates class used to support the generation of the
+ * RoomBuilder class
+ */
 public class Coordinates {
     private final int x;
     private final int y;
     private final JSONArray array = new JSONArray();
 
+    /**
+     * Constructor method for custom class coordinates, take in x and y int
+     * and populate the JSONArray coordination used for the RoomBuilder class
+     * @param x
+     * @param y
+     */
     public Coordinates(int x, int y) {
         this.x = x;
         this.y = y;
@@ -19,6 +29,11 @@ public class Coordinates {
         this.array.put(y);
     }
 
+    /**
+     * Constructor method for custom class coordinates, take in a
+     * String of coordination e.g 1,2 to populate the coordinates array
+     * @param coordinateText
+     */
     public Coordinates(String coordinateText) {
         List<String> coorList = new ArrayList<String>(Arrays.asList(coordinateText.split(",")));
         this.x = Integer.parseInt(coorList.get(0).toString());
@@ -27,10 +42,13 @@ public class Coordinates {
         this.array.put(y);
     }
 
-    public JSONArray get(){
-        return this.array;
-    }
-
+    /**
+     * Constructor method for custom class coordinates, take in an int for
+     * size limit and generate a random combination of coordinate between 0 and size
+     * NOTE: This method works best when size is at least 10, anything bellow 10 has
+     * a very high chance of duplication for the coordinates being generated
+     * @param size
+     */
     public Coordinates(int size){
         Random rand = new Random();
         this.x = rand.nextInt(size);
@@ -39,11 +57,27 @@ public class Coordinates {
         this.array.put(y);
     }
 
-    public int getX(){
-        return x;
+    /**
+     * Get method return the coordinate array
+     * @return array
+     */
+    public JSONArray get(){
+        return this.array;
     }
 
+    /**
+     * This method returns the x of the coordinate
+     * @return x
+     */
+    public int getX(){
+        return this.x;
+    }
+
+    /**
+     * THis method return the y of the coordinate
+     * @return y
+     */
     public int getY(){
-        return y;
+        return this.y;
     }
 }
--- src/test/java/utils/RoomBuilder.java
+++ src/test/java/utils/RoomBuilder.java
@@ -10,6 +10,15 @@ public class RoomBuilder {
     private final JSONArray dirtPatches;
     private final String instructions;
 
+
+    /**
+     * This constructor method takes in roomDimension, startingCoordinates
+     * dirtPatches and instruction for the hover bot and store it
+     * @param roomDimension
+     * @param startingCoordinates
+     * @param dirtPatches
+     * @param instruction
+     */
     public RoomBuilder(Coordinates roomDimension, Coordinates startingCoordinates, JSONArray dirtPatches, String instruction) {
         this.roomDimension = roomDimension;
         this.startingCoordinates = startingCoordinates;
@@ -17,6 +26,14 @@ public class RoomBuilder {
         this.instructions = instruction;
     }
 
+    /**
+     * This constructor method takes in roomDimension, startingCoordinates and instruction
+     * but generate a random number of dirt patches based on the int defined by the user
+     * @param roomDimension
+     * @param startingCoordinates
+     * @param dirtPatches
+     * @param instruction
+     */
     public RoomBuilder(Coordinates roomDimension, Coordinates startingCoordinates, int dirtPatches, String instruction) {
         this.roomDimension = roomDimension;
         this.startingCoordinates = startingCoordinates;
@@ -24,6 +41,14 @@ public class RoomBuilder {
         this.instructions = instruction;
     }
 
+    /**
+     * This constructor method takes in roomDimension and startingCoordinates
+     * but generate a random number of dirt patches based on the int defined by the user
+     * and instruction is to automatically advance to the top right corner of the room
+     * @param roomDimension
+     * @param startingCoordinates
+     * @param dirtPatches
+     */
     public RoomBuilder(Coordinates roomDimension,Coordinates startingCoordinates, int dirtPatches){
         this.roomDimension = roomDimension;
         this.dirtPatches = generateRandomDirtPatches(dirtPatches);
@@ -31,6 +56,14 @@ public class RoomBuilder {
         this.instructions = generateInstruction();
     }
 
+    /**
+     * This constructor method takes in roomDimension, startingCoordinate and dirPatches
+     * coordinates and generate the instruction to advance to the top right corner
+     * of the room
+     * @param roomDimension
+     * @param startingCoordinates
+     * @param dirtPatches
+     */
     public RoomBuilder(Coordinates roomDimension, Coordinates startingCoordinates, JSONArray dirtPatches) {
         this.roomDimension = roomDimension;
         this.startingCoordinates = startingCoordinates;
@@ -38,6 +71,12 @@ public class RoomBuilder {
         this.instructions = generateInstruction();
     }
 
+    /**
+     * Method to generate random number coordinates for dirt patches based
+     * on the input int
+     * @param dirtPatches
+     * @return randomDirtPatches
+     */
     private JSONArray generateRandomDirtPatches(int dirtPatches){
         JSONArray randomDirtPatches = new JSONArray();
         for (int a=0; a < dirtPatches; a++){
@@ -46,6 +85,21 @@ public class RoomBuilder {
         return randomDirtPatches;
     }
 
+    /**
+     * This method is used mainly to scan the room generated and assumed that
+     * the default hovering location is at 0,0.
+     *
+     * This will go from the starting position 0,0 moving right (E) to x=max
+     * then move up (N) and move left (W) until x=0. The process will repeat until
+     * y get to y-max for odd y-max and to y-max - 1 for even y-max. The function
+     * also assumed that since the starting position is always at 0,0 even line
+     * will have the bot go right and odd line will have the bot go left
+     *
+     * NOTE: Due to the nature of the program (coordinates does not change when get
+     * to the x/y limits). The method can also be used to move to the top right of the
+     * room.
+     * @return steps
+     */
     private String generateInstruction(){
         int totalTiles = (this.roomDimension.getX() * this.roomDimension.getY()) - 1;
         String steps ="E";
@@ -79,6 +133,12 @@ public class RoomBuilder {
         System.out.println(steps);
         return steps;
     }
+
+    /**
+     * Build the JSON request based on roomDimension, startingCoordinates
+     * dirtPataches and instructions
+      * @return JSON Request Body
+     */
     public String build(){
         System.out.println(new JSONObject()
                 .put("roomSize", roomDimension.get())
